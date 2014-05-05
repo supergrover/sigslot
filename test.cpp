7221d38d9972fc1ca18a1f1d1d1b75df8a7c4ba1
Leon Colijn <lcolijn@xs4all.nl>
2014-05-05 23:03:43 +0200

    Simplify, remove Object base class for emitter

--- test.cpp
+++ test.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 #include "signal.h"
 
-class Emitter: public Object
+class Emitter
 {
     public:
         Signal<int> the_signal;
 
         void do_emit(int value)
         {
-            the_signal.invoke(value);
+            the_signal.emit(value);
         }
 };
 
@@ -25,11 +25,14 @@ class Receiver: public Object
 
 int main(int argc, char* argv[])
 {
+    // Create both an emitting and receiving instance
     Emitter em;
     Receiver recv;
 
+    // Bind the slot to the signal
     em.the_signal.bind(&Receiver::slot, &recv);
 
+    // Emit
     em.do_emit(42);
 
     return EXIT_SUCCESS;
